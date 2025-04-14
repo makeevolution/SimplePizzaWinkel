fab9221b6b8405942192d7e92b28884b6bfe220d
Aldo <aldo_hasibuan@yahoo.com>
2025-04-14 18:09:18 +0200

    add make commands to push to dockerhub, move deployment related files to deployment folder, update docs, add docker compose file for deployment using images from dockerhub

---
+++ deployment/README.md
@@ -0,0 +1,5 @@
+# Deployment
+
+- See [Overview](../docs/overview.md) for a high-level overview of the deployment.
+- `components` folder contain dapr settings and configuration
+- `nginx` folder contain reverse proxy settings
--- src/PlantBasedPizza.Recipes/applications/PlantBasedPizza.Recipes.Infrastructure/InfrastructureConstants.cs
+++ src/PlantBasedPizza.Recipes/applications/PlantBasedPizza.Recipes.Infrastructure/InfrastructureConstants.cs
@@ -2,5 +2,5 @@ namespace PlantBasedPizza.Recipes.Infrastructure;
 
 public static class InfrastructureConstants
 {
-    public static string TableName => Environment.GetEnvironmentVariable("TABLE_NAME") ?? "plant-based-pizza";
+    public static string TableName => Environment.GetEnvironmentVariable("TABLE_NAME") ?? "simple-pizza-winkel";
 }
